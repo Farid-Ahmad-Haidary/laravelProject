71f01c86d8a676566a889b7f4a592322eaefebd9
Farid Ahmad Haidary <faridahmadhaidary01@gmail.com>
2025-06-28 17:02:07 +0430

    seeding file is complated

--- database/seeders/UserTableseeder.php
+++ database/seeders/UserTableseeder.php
@@ -4,14 +4,34 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use function Laravel\Prompts\table;
+use Illuminate\Support\Facades\DB;
+
+
 
 class UserTableseeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+                'status' => '1',
+            ],
+            [
+                'name' => 'user',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('user123'),
+                'role' => 'user',
+                'status' => '1',
+            ]
+        ]);
     }
 }
