68c655101cedc8e468b9861f6f1558234f8b96ca
Farid Ahmad Haidary <faridahmadhaidary01@gmail.com>
2025-06-28 16:32:21 +0430

    Breeze is installed and some tables are added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
             $table->string('email')->unique();
+            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->enum('status', ['0', '1'])->default('0');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
